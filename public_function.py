d3b4887ae1f415b058f256b87789c77a9158cbe8
tzengshinfu <tzengshinfu@gmail.com>
2018-10-16 17:29:20 +0800

    developing

--- public_function.py
+++ public_function.py
@@ -5,19 +5,19 @@ from lxml import etree
 
 class PublicFunction():
     pass
-    
+
     def get_browser_headers(self, referer_url):
         """取得不同的瀏覽器Request Header"""
         if 'vanguard' in referer_url:
             vanguard_headers = {
                 'DNT':
-                '1', 
+                '1',
                 'Accept-Encoding':
-                'gzip, deflate, br', 
+                'gzip, deflate, br',
                 'Accept-Language':
-                'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7', 
+                'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
                 'User-Agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36', 
+                'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
                 'Accept':
                 '*/*',
                 'Referer':
@@ -28,8 +28,16 @@ class PublicFunction():
             headers = vanguard_headers
         else:
             chrome_header = {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Cache-Control': 'max-age=0',
+                'Connection': 'keep-alive',
+                'DNT': '1',
+                'Host': 'www.twse.com.tw',
+                'Upgrade-Insecure-Requests': '1',
                 'user-agent':
-                'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+                'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
             }
             headers = chrome_header
 
