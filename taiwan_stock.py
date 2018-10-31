f28fd999e89a718192b126da3664ce321a10a745
tzengshinfu <tzengshinfu@gmail.com>
2018-11-01 00:13:57 +0800

    developing

--- taiwan_stock.py
+++ taiwan_stock.py
@@ -5,54 +5,59 @@ import time
 class TaiwanStock():
     fetcher = webpage_fetcher.WebpageFetcher()
 
-    def get(self):
-        # self.get_stock_codes()
-        self.get_stock_eps()
+    def get_stock_details(self):
+        stock_codes = self.get_stock_codes()
+        stock_eps = self.get_stock_eps()
+        print(stock_codes)
+        print(stock_eps)
         self.fetcher.exit()
 
     def get_stock_codes(self):
         """取得台股上巿股票列表"""
         self.fetcher.go_to('http://www.twse.com.tw/zh/stockSearch/stockSearch')
-        stock_codes = []
+        result = []
         stock_links = self.fetcher.find_elements('//table[@class="grid"]//a')
         for link in stock_links:
             code = link.text
-            stock_codes.append([code[0:4], code[4:]])
-        return stock_codes
+            result.append([code[0:4], code[4:]])
+        return result
 
     def get_stock_eps(self):
-        def get_years(self):
-            years = []
+        def get_eps_years(self):
+            """取得EPS年度"""
+            result = []
             year_tags = self.fetcher.find_elements('//select[@id="ctl00_ContentPlaceHolder1_D3"]/option')
-            for year in year_tags:
-                years.append(year.text)
-            return years
+            for tag in year_tags:
+                result.append(tag.text)
+            return result
 
-        def get_eps_data(self):
-            """取得台股上巿股票EPS"""
-            stock_eps = []
+        def get_eps_data(self, year):
+            """取得EPS資料"""
+            result = []
             eps_records = self.fetcher.find_elements('//table[@id="ctl00_ContentPlaceHolder1_GridView1"]//tr[not(@align)]')
             for record in eps_records:
                 eps = []
-                eps.append('2018Q3')
+                eps.append(year)
                 eps_fields = record.find_elements_by_tag_name('td')
                 for field in eps_fields:
                     eps.append(field.text)
-                stock_eps.append(eps)
-            return stock_eps
+                result.append(eps)
+            return result
 
-        def get_table_data(self, year):
-            """取得台股上巿股票EPS"""
+        def get_eps_table_content(self, year):
+            """取得EPS表格內容作為判斷是否已載入完成"""
             return self.fetcher.find_element('//table[@id="ctl00_ContentPlaceHolder1_GridView1"]').text
 
         self.fetcher.go_to('https://www.cnyes.com/twstock/financial4.aspx')
-        years = get_years(self)
-        pre_eps_table_text = ''
-        for year in years:
+        eps_years = get_eps_years(self)
+        previous_eps_table_content = ''
+        s = []
+        for year in eps_years:
             self.fetcher.find_element('//select[@id="ctl00_ContentPlaceHolder1_D3"]/option[text()="' + year + '"]').click()
-            current_eps_table_text = get_table_data(self, year)
-            while current_eps_table_text == pre_eps_table_text:
+            current_eps_table_content = get_eps_table_content(self, year)
+            while current_eps_table_content == previous_eps_table_content:  # 重新執行直到取得當年度的資料
                 time.sleep(0.2)
-                current_eps_table_text = get_table_data(self, year)
-
-            pre_eps_table_text = get_table_data(self, year)
+                current_eps_table_content = get_eps_table_content(self, year)
+            s.append(get_eps_data(self, year))
+            previous_eps_table_content = get_eps_table_content(self, year)
+        return s
